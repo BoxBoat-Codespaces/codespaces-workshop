b57b795ae99e33dc4dd11ddc6e421dc781e1eb9d
Daniel Koch <dkoch84@gmail.com>
2022-06-29 19:46:01 -0500

    storage account updated for test and for workshopt

--- setup-test/test.tf
+++ setup-test/test.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "codespaces-demo-resources"
-    storage_account_name = "codespacesstate"
+    storage_account_name = "codespacesstate1"
     container_name       = "codespacesstate"
     key                  = "setuptest.state"
   }
--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "codespaces-demo-resources"
-    storage_account_name = "codespacesstate"
-    container_name       = "codespacesstate"
-    key                  = "<USERNAME>.state"
+    resource_group_name  = ""
+    storage_account_name = ""
+    container_name       = ""
+    key                  = ""
   }
 }
