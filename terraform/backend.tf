1dd5c69fc73424a460feb0b1547e88b73a5d01df
Daniel Koch <dkoch84@gmail.com>
2022-06-29 19:46:01 -0500

    hardcoding state things for the most part

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = ""
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
+    resource_group_name  = "codespaces-demo-resources"
+    storage_account_name = "codespacesstate1"
+    container_name       = "codespacesstate"
+    key                  = "<USERNAME>.state"
   }
 }
